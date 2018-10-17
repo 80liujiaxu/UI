d2e3fd172a353474719823bfbdc9ad40c493ec48
80liujiaxu <809465118@qq.com>
2018-10-17 18:24:33 +0800

    第N次上传

---
+++ src/main/java/tk/mybatis/springboot/dao/CityMapper.java
@@ -0,0 +1,24 @@
+package tk.mybatis.springboot.dao;
+
+
+
+
+
+
+
+
+
+
+
+
+import tk.mybatis.springboot.model.City;
+import tk.mybatis.springboot.util.MyMapper;
+
+/**
+ * @author liuzh_3nofxnp
+ * @since 2016-01-22 22:17
+ */
+public interface CityMapper extends MyMapper<City> {
+	
+	
+}
